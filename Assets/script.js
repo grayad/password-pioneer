7ac51236c151f9c8fccc5faedea86b6ffac874ac
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-05-01 17:10:54 -0400

    added the 4 base arrays

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y","z"];
 
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+var specialChar = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+
+var passwordPrompts = function () {
+  window.alert("Welcome to Password Pioneer! Answer the following prompts to generate your password. Click OK to continue.")
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +26,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+
