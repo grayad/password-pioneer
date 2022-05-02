f2603199e95d1dd1dac67afc52ac1e9d16960bff
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-05-02 12:02:02 -0400

    add new array based on selected PW criteria

--- Assets/script.js
+++ Assets/script.js
@@ -13,6 +13,8 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
 var specialChar = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+var passwordArray = [""];
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -23,15 +25,35 @@ function writePassword() {
   // Prompt user input for number of characters in the password (>8)
   var charAmt= window.prompt("How many character would you like your password to contain?");
   if (charAmt<8) {
-      window.alert("Password must be at least 8 characters.");
-      var charAmt= window.prompt("How many character would you like your password to contain?");
-  };
+    window.alert("Password must be at least 8 characters.");
+    var charAmt= window.prompt("How many character would you like your password to contain?");
+  }
+  console.log(charAmt);
 
   // Ask user to confirm desired password criteria
   var specialConfirm= window.confirm("Click OK to confirm including special characters.");
+  if (specialConfirm) {
+    passwordArray.push.apply(passwordArray,specialChar);
+  }
+
   var numConfirm= window.confirm("Click OK to confirm including numeric characters.");
+  if (numConfirm) {
+    passwordArray.push.apply(passwordArray,numbers);
+  }
+
   var lowerConfirm= window.confirm("Click OK to confirm including lowercase characters.");
+  if (lowerConfirm) {
+    passwordArray.push.apply(passwordArray,lowercase);
+  }
+
+
   var upperConfirm= window.confirm("Click OK to confirm including uppercase characters.");
+  if (upperConfirm) {
+    passwordArray.push.apply(passwordArray,uppercase);
+  }
+
+  console.log(passwordArray);
+
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
